6010d1c96a434db85f0333a42a076c67c6971d4a
Ehud-Karavani <ehud.karavani@ibm.com>
2024-03-05 10:36:02 +0200

    Add trailing `_` to post-init attributes
    Follow scikit-learn's convention for post-fit attributes.
    
    Consider if all attributes (especially raw data) are worth
    saving as attributes.

--- gmm.py
+++ gmm.py
@@ -57,45 +57,45 @@ class GMMEstimatorScipy(GMMEstimator):
         """
         Quadratic form to be minimized.
         """
-        moments = self.moment_cond(self.z, self.y, self.x, beta)
+        moments = self.moment_cond(self.z_, self.y_, self.x_, beta)
         if self.weighting_matrix == "optimal":
-            self.W = self.optimal_weighting_matrix(moments)
+            self.W_ = self.optimal_weighting_matrix(moments)
         else:
-            self.W = np.eye(moments.shape[1])
+            self.W_ = np.eye(moments.shape[1])
         mavg = moments.mean(axis=0)
-        return mavg.T @ self.W @ mavg
+        return mavg.T @ self.W_ @ mavg
 
     def optimal_weighting_matrix(self, moments):
         """
         Optimal Weight matrix
         """
-        return np.linalg.inv((1 / self.n) * (moments.T @ moments))
+        return np.linalg.inv((1 / self.n_) * (moments.T @ moments))
 
     def fit(self, z, y, x, verbose=False, fit_method=None):
         if fit_method is None:
             fit_method = "L-BFGS-B"
-        self.z, self.y, self.x = z, y, x
-        self.n, self.k = x.shape
+        self.z_, self.y_, self.x_ = z, y, x
+        self.n_, self.k_ = x.shape
         # minimize the objective function
         result = scipy.optimize.minimize(
             self.gmm_objective,
-            x0=np.random.rand(self.k),
+            x0=np.random.rand(self.k_),
             method=fit_method,
             options={"disp": verbose},
         )
-        self.theta = result.x
+        self.theta_ = result.x
         # Standard error calculation
         try:
-            self.Gamma = self.jacobian_moment_cond()
-            self.vθ = np.linalg.inv(self.Gamma.T @ self.W @ self.Gamma)
-            self.std_errors = np.sqrt(self.n * np.diag(self.vθ))
+            self.Gamma_ = self.jacobian_moment_cond()
+            self.vθ_ = np.linalg.inv(self.Gamma_.T @ self.W_ @ self.Gamma_)
+            self.std_errors_ = np.sqrt(self.n_ * np.diag(self.vθ_))
         except:
-            self.std_errors = None
+            self.std_errors_ = None
 
     def jacobian_moment_cond(self):
         """Jacobian of the moment condition"""
-        self.jac_est = -self.z.T @ self.x
-        return self.jac_est
+        self.jac_est_ = -self.z_.T @ self.x_
+        return self.jac_est_
 
 
 # %%
@@ -110,61 +110,61 @@ class GMMEstimatorTorch(GMMEstimator):
         """
         Quadratic form to be minimized.
         """
-        moments = self.moment_cond(self.z, self.y, self.x, beta)
+        moments = self.moment_cond(self.z_, self.y_, self.x_, beta)
         if self.weighting_matrix == "optimal":
-            self.W = self.optimal_weighting_matrix(moments)
+            self.W_ = self.optimal_weighting_matrix(moments)
         else:
-            self.W = torch.eye(moments.shape[1])
+            self.W_ = torch.eye(moments.shape[1])
         mavg = moments.mean(axis=0)
         return torch.matmul(
             mavg.unsqueeze(-1).T,
-            torch.matmul(self.W, mavg),
+            torch.matmul(self.W_, mavg),
         )
 
     def optimal_weighting_matrix(self, moments):
         """
         Optimal Weight matrix
         """
-        return torch.inverse((1 / self.n) * torch.matmul(moments.T, moments))
+        return torch.inverse((1 / self.n_) * torch.matmul(moments.T, moments))
 
     def fit(self, z, y, x, verbose=False, fit_method=None):
         if fit_method is None:
             fit_method = "l-bfgs"
         # minimize blackbox using pytorch
-        self.z, self.y, self.x = (
+        self.z_, self.y_, self.x_ = (
             torch.tensor(z, dtype=torch.float64),
             torch.tensor(y, dtype=torch.float64),
             torch.tensor(x, dtype=torch.float64),
         )
-        self.n, self.k = x.shape
+        self.n_, self.k_ = x.shape
         beta_init = torch.tensor(
-            np.random.rand(self.k), dtype=torch.float64, requires_grad=True
+            np.random.rand(self.k_), dtype=torch.float64, requires_grad=True
         )
         result = torchmin.minimize(
             self.gmm_objective, beta_init, method=fit_method, tol=1e-5, disp=verbose
         )
-        self.W = self.W.detach().numpy()
+        self.W_ = self.W_.detach().numpy()
         # solution
-        self.theta = result.x
+        self.theta_ = result.x
 
         # Standard error calculation
         try:
-            self.Gamma = self.jacobian_moment_cond()
-            self.vθ = np.linalg.inv(self.Gamma.T @ self.W @ self.Gamma)
-            self.std_errors = np.sqrt(self.n * np.diag(self.vθ))
+            self.Gamma_ = self.jacobian_moment_cond()
+            self.vθ_ = np.linalg.inv(self.Gamma_.T @ self.W_ @ self.Gamma_)
+            self.std_errors_ = np.sqrt(self.n_ * np.diag(self.vθ_))
         except:
-            self.std_errors = None
+            self.std_errors_ = None
 
     def jacobian_moment_cond(self):
         """
         Jacobian of the moment condition
         """
         # forward mode automatic differentiation wrt 3rd arg (parameter vector)
-        self.jac = torch.func.jacfwd(self.moment_cond, argnums=3)
-        self.jac_est = (
-            self.jac(self.z, self.y, self.x, self.theta).sum(axis=0).detach().numpy()
+        self.jac_ = torch.func.jacfwd(self.moment_cond, argnums=3)
+        self.jac_est_ = (
+            self.jac_(self.z_, self.y_, self.x_, self.theta_).sum(axis=0).detach().numpy()
         )
-        return self.jac_est
+        return self.jac_est_
 
 
 _BACKENDS = {
